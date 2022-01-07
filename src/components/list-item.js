d5988560979144de99ec652f558f7780646dd9f3
BLooDBRothER <computertechnology446@gmail.com>
2022-01-07 17:10:43 +0530

    Add undo img

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -1,7 +1,6 @@
-import React from "react";
-
 import completedIcon from "../Assets/completed.svg";
 import deleteIcon from "../Assets/delete.svg";
+import uncompleted from "../Assets/uncompleted.svg"
 
 const ListItem = ({todo, todoList, setTodoList}) => {
     const deleteList = (e) => {
@@ -19,7 +18,7 @@ const ListItem = ({todo, todoList, setTodoList}) => {
         <div className={`list ${todo.isCompleted? 'true' : 'false'}`}>
             <h4 className="list-value">{todo.value}</h4>
             <div className="list-ic">
-                <img src={completedIcon} className="list-completed" onClick={toggleListCompleted} alt="Completed" />
+                <img src={`${todo.isCompleted ? uncompleted : completedIcon}`} className="list-completed" onClick={toggleListCompleted} alt="Completed" />
                 <img src={deleteIcon} className="list-delete" onClick={deleteList} alt="Remove" />
             </div>
         </div>
